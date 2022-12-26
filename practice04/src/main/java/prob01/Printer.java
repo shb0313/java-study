191845a00cf0d5dee07f3004922e1d648b00d5b0
shb7815 <shb7815@gmail.com>
2022-12-26 10:03:31 +0900

    generic Method

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -1,19 +1,34 @@
 package prob01;
 
 public class Printer {
+/*
 	public void println(int num) {
 		System.out.println(num);
 	}
+	
 	public void println(boolean bool) {
 		System.out.println(bool);
 	}
+	
 	public void println(double num) {
 		System.out.println(num);
 	}
+	
 	public void println(String string) {
 		System.out.println(string);
 	}
+*/	
+	public <T> void println(T... ts) {
+		for(T t : ts) {
+			System.out.println(t);
+		}
+	}
 	
-	
-	
+	public int sum(Integer... nums) {
+		int s = 0;
+		for(Integer n : nums) {
+			s += n;
+		}
+		return s;
+	}
 }
