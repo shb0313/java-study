302b7d99d9c92c8cbca5df6b991fed322a971155
shb7815 <shb7815@gmail.com>
2022-12-28 17:06:41 +0900

    NSLookup

--- network/src/main/java/util/NSLookup.java
+++ network/src/main/java/util/NSLookup.java
@@ -2,13 +2,30 @@ package util;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class NSLookup {
 
 	public static void main(String[] args) {
-		 
+
 		try {
-			InetAddress[] inetAddresses = InetAddress.getAllByName("www.douzone.com");
+
+			while (true) {
+				System.out.print(">");
+				Scanner sc = new Scanner(System.in);
+				String domainName = sc.nextLine();
+
+				InetAddress[] inetAddresses = InetAddress.getAllByName(domainName);
+
+				for (int i = 0; i < inetAddresses.length; i++) {
+					System.out.println(inetAddresses[i].getHostName() + " : " + inetAddresses[i].getHostAddress());
+				}
+
+				if (domainName == "exit") {
+					break;
+				}
+
+			}
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
