f55b928885ea41e089f78c71ebd685e8da5dbc8b
shb7815 <shb7815@gmail.com>
2022-12-30 11:19:27 +0900

    UDP

--- network/src/main/java/udp/UDPEchoServer.java
+++ network/src/main/java/udp/UDPEchoServer.java
@@ -8,42 +8,44 @@ import java.net.SocketException;
 public class UDPEchoServer {
 	public static final int PORT = 5000;
 	public static final int BUFFER_SIZE = 256;
-
+	
 	public static void main(String[] args) {
 		DatagramSocket socket = null;
-	
+		
 		try {
-			// 1. 소켓생성
+			// 1. 소켓 생성
 			socket = new DatagramSocket(PORT);
-
+			
 			while(true) {
 				// 2. 데이터 수신
 				DatagramPacket rcvPacket = new DatagramPacket(new byte[BUFFER_SIZE], BUFFER_SIZE);
-				socket.receive(rcvPacket);
+				socket.receive(rcvPacket); //Blocking
 				
 				byte[] rcvData = rcvPacket.getData();
 				int offset = rcvPacket.getLength();
 				String message = new String(rcvData, 0, offset, "utf-8");
+	
+				System.out.println("[UDP Echo Server] received:" + message);
 				
-				System.out.println("[server] received:" + message);
-				
-				//3. 데이터 통신
+				// 3. 데이터 송신
 				byte[] sndData = message.getBytes("utf-8");
-				DatagramPacket sndPacket = new DatagramPacket(sndData, sndData.length, rcvPacket.getAddress(), rcvPacket.getPort());
+				DatagramPacket sndPacket = new DatagramPacket(
+					sndData,
+					sndData.length,
+					rcvPacket.getAddress(),
+					rcvPacket.getPort());
 				
 				socket.send(sndPacket);
-				
 			}
-			
-		} catch (SocketException e) {
-			System.out.println("error: " + e);
-		} catch (IOException e) {
-		System.out.println("error: " + e);
+		} catch(SocketException e) {
+			System.out.println("[UDP Echo Server] error: " + e);
+		} catch(IOException e) {
+			System.out.println("[UDP Echo Server] error: " + e);
 		} finally {
-			if (socket != null && !socket.isClosed()) {
+			if(socket != null && !socket.isClosed()) {
 				socket.close();
 			}
 		}
-
 	}
+
 }
