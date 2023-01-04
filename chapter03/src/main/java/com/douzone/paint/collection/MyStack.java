ca42ba8210af0108091c0fedf461a03f83381dcb
shb7815 <shb7815@gmail.com>
2023-01-04 20:44:15 +0900

    stack

--- chapter03/src/main/java/com/douzone/paint/collection/MyStack.java
+++ chapter03/src/main/java/com/douzone/paint/collection/MyStack.java
@@ -1,10 +1,12 @@
 package com.douzone.paint.collection;
 
+import java.util.Arrays;
+
 public class MyStack<T> {
 	private T[] buffer;
 	private int top;
 	
-	//@SuppressWarnings("unchecked")
+	@SuppressWarnings("unchecked")
 	public MyStack(int i) {
 		buffer = (T[])new Object[i];
 		top = -1;
@@ -33,7 +35,7 @@ public class MyStack<T> {
 		return top == -1;
 	}
 
-	//@SuppressWarnings("unchecked")
+	@SuppressWarnings("unchecked")
 	public void resize() {
 		T[] temp = (T[])new Object[buffer.length * 2];
 		for (int i = 0; i <= top; i++) {
